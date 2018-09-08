81ba8c03266f675baa3717a6e41966d61e330245
Administrator <619774333@qq.com>
2018-09-08 21:11:11 +0800

    安全校验

--- src/main/java/com/hoperun/qkl/fileserve/controller/LoginResource.java
+++ src/main/java/com/hoperun/qkl/fileserve/controller/LoginResource.java
@@ -4,7 +4,6 @@ package com.hoperun.qkl.fileserve.controller;
 import com.hoperun.qkl.fileserve.controller.vm.LoginVM;
 import com.hoperun.qkl.fileserve.repository.UserRepository;
 import com.hoperun.qkl.fileserve.sercurity.JwtTokenUtil;
-import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -13,8 +12,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -45,8 +42,19 @@ public class LoginResource {
                     new UsernamePasswordAuthenticationToken(loginVM.getUsername(), loginVM.getPassword()));
             SecurityContextHolder.getContext().setAuthentication(authentication);
             String token = jwtTokenUtil.generateToken(loginVM.getUsername());
-            response.addHeader(tokenHeader,"Bearer"+token);
+            response.addHeader(tokenHeader,"Bearer "+token);
             return new ResponseEntity<>(userRepository.findByLogin(loginVM.getUsername()),HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @RequestMapping(value = "/loginOut", method = RequestMethod.POST)
+    public ResponseEntity<?> loginOut(HttpServletResponse response) {
+        try {
+
+            return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/com/hoperun/qkl/fileserve/sercurity/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/hoperun/qkl/fileserve/sercurity/JwtAuthenticationTokenFilter.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
@@ -32,20 +33,18 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+
         String authToken = request.getHeader(this.tokenHeader);
-        
+        if(!StringUtils.isEmpty(authToken) && authToken.length() > 7){
+            authToken = authToken.substring(7);
+        }
         String username = jwtTokenUtil.getUsernameFromToken(authToken);
 
-        logger.info("checking authentication for user " + username);
-
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-
+        if (!StringUtils.isEmpty(username) && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-
             if (jwtTokenUtil.validateToken(authToken, userDetails)) {
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                logger.info("authenticated user " + username + ", setting security context");
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
