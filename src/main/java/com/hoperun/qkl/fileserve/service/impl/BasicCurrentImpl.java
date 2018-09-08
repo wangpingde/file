35610cec98e3d261f1e2990b16b7b3b314bdedcb
Administrator <619774333@qq.com>
2018-09-08 22:09:11 +0800

    安全校验

--- src/main/java/com/hoperun/qkl/fileserve/service/impl/BasicCurrentImpl.java
+++ src/main/java/com/hoperun/qkl/fileserve/service/impl/BasicCurrentImpl.java
@@ -3,17 +3,19 @@ package com.hoperun.qkl.fileserve.service.impl;
 import com.hoperun.qkl.fileserve.domain.User;
 import com.hoperun.qkl.fileserve.repository.UserRepository;
 import com.hoperun.qkl.fileserve.service.BasicCurrentUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BasicCurrentImpl implements BasicCurrentUser {
 
+    @Autowired
     private UserRepository userRepository;
 
     @Override
     public User getCurrentUser() {
         return userRepository.findByLogin(
-                SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
+                SecurityContextHolder.getContext().getAuthentication().getName());
     }
 }
