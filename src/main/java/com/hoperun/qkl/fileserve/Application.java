bf15be063d6f0ed3387a3019827fc068a221353f
Administrator <619774333@qq.com>
2018-09-08 15:55:38 +0800

    保存文件信息

--- src/main/java/com/hoperun/qkl/fileserve/Application.java
+++ src/main/java/com/hoperun/qkl/fileserve/Application.java
@@ -2,11 +2,9 @@ package com.hoperun.qkl.fileserve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.hoperun.qkl.fileserve.repository")
 public class Application {
 
     public static void main(String[] args) {
