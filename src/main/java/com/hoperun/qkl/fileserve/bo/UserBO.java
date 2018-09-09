6b2e07b946c4f94041431899a3e64f615e794855
Administrator <619774333@qq.com>
2018-09-09 12:34:34 +0800

    安全校验

---
+++ src/main/java/com/hoperun/qkl/fileserve/bo/UserBO.java
@@ -0,0 +1,91 @@
+package com.hoperun.qkl.fileserve.bo;
+
+import com.hoperun.qkl.fileserve.domain.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+public class UserBO implements UserDetails {
+    private String login;
+
+    private String password;
+
+    private boolean activated;
+
+    private Set<UserRole> roles;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActivated() {
+        return activated;
+    }
+
+    public void setActivated(boolean activated) {
+        this.activated = activated;
+    }
+
+    public Set<UserRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<UserRole> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> auths = new ArrayList<>();
+        Set<UserRole> roles = this.getRoles();
+        for (UserRole role : roles) {
+            auths.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return auths;
+
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.activated;
+    }
+
+}
--- src/main/java/com/hoperun/qkl/fileserve/sercurity/JwtUserDetailsServiceImpl.java
+++ src/main/java/com/hoperun/qkl/fileserve/sercurity/JwtUserDetailsServiceImpl.java
@@ -1,16 +1,15 @@
 package com.hoperun.qkl.fileserve.sercurity;
 
 
+import com.hoperun.qkl.fileserve.bo.UserBO;
 import com.hoperun.qkl.fileserve.domain.User;
 import com.hoperun.qkl.fileserve.repository.UserRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
 
 @Service
 public class JwtUserDetailsServiceImpl implements UserDetailsService {
@@ -21,13 +20,12 @@ public class JwtUserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
         User user = userRepository.findByLogin(login);
+        UserBO userBO = new UserBO();
+        BeanUtils.copyProperties(user,userBO);
         if (user == null) {
             throw new UsernameNotFoundException(String.format("No user found with login '%s'.", login));
         } else {
-            return new org.springframework.security.core.userdetails.User(
-                    user.getLogin(),user.getPassword(),
-                    Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"),
-                                  new SimpleGrantedAuthority("ROLE_USER")));
+            return  userBO;
         }
     }
 }
